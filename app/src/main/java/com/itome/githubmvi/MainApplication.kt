230f58e735bf5f3df76d60981ecc19a8e23b4952
itome <t.t.itm.0403@gmail.com>
2018-02-11 09:09:02 +0900

    Install realm

---
+++ app/src/main/java/com/itome/githubmvi/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.itome.githubmvi
+
+import android.app.Application
+import io.realm.Realm
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
