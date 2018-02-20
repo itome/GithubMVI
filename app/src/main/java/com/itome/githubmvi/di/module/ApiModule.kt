c9858b9ed4bfb4243390468ac775e933550e4ee1
itome <t.t.itm.0403@gmail.com>
2018-02-21 08:44:48 +0900

    Enable http logggin

--- app/src/main/java/com/itome/githubmvi/di/module/ApiModule.kt
+++ app/src/main/java/com/itome/githubmvi/di/module/ApiModule.kt
@@ -4,6 +4,8 @@ import com.itome.githubmvi.data.datastore.ApiService
 import com.itome.githubmvi.data.datastore.LoginService
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,11 +17,14 @@ class ApiModule {
     @Singleton
     @Provides
     fun provideApiService(): ApiService = Retrofit.Builder()
-                .baseUrl("https://api.github.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-                .create(ApiService::class.java)!!
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                    .build())
+            .build()
+            .create(ApiService::class.java)!!
 
     @Singleton
     @Provides
@@ -27,6 +32,9 @@ class ApiModule {
             .baseUrl("https://github.com")
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+                    .build())
             .build()
             .create(LoginService::class.java)!!
 }
