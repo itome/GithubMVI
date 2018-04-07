ecc1171acb8d9429bee79774934a4b858d7241c7
itome <t.t.itm.0403@gmail.com>
2018-04-07 20:35:13 +0900

    Implement mergeProcessor in MviProcessorHolder

--- app/src/main/java/com/itome/githubmvi/mvibase/MviProcessorHolder.kt
+++ app/src/main/java/com/itome/githubmvi/mvibase/MviProcessorHolder.kt
@@ -1,7 +1,9 @@
 package com.itome.githubmvi.mvibase
 
+import com.itome.githubmvi.extensions.ofType
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
+import kotlin.reflect.KClass
 
 abstract class MviProcessorHolder<A : MviAction, R : MviResult> {
 
@@ -13,4 +15,28 @@ abstract class MviProcessorHolder<A : MviAction, R : MviResult> {
         ObservableTransformer { actions ->
             actions.flatMap(processorFunction)
         }
+
+    @Suppress("UNCHECKED_CAST")
+    protected fun mergeProcessor(
+        vararg processors: Pair<ObservableTransformer<out A, out R>, KClass<out A>>
+    ): ObservableTransformer<A, R> {
+        return ObservableTransformer { actions ->
+            actions.publish { shared ->
+                Observable.merge<R>(
+                    processors.map { (processor, clazz) ->
+                        shared.ofType(clazz)
+                            .compose(processor as ObservableTransformer<in Any, out R>)
+                    }
+                ).mergeWith(
+                    shared.filter({ v ->
+                        processors.all { (_, clazz) -> v::class != clazz }
+                    }).flatMap { w ->
+                        Observable.error<R>(
+                            IllegalArgumentException("Unknown Action type: $w")
+                        )
+                    }
+                )
+            }
+        }
+    }
 }
